bc484aaa361fbdd5e002fe96b72ecd440ea13765
aysaafrin <86862857+aysaafrin@users.noreply.github.com>
2022-01-05 15:55:22 +0530

    Update Book.java

--- src/main/java/com/mindtree/cap/Entity/Book.java
+++ src/main/java/com/mindtree/cap/Entity/Book.java
@@ -4,6 +4,7 @@ public class Book {
   private int id;
   private String name;
   private String author;
+ private int bookpagenum;
 public Book(int id, String name, String author) {
 	super();
 	this.id = id;

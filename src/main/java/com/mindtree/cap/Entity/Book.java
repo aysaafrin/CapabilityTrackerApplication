ef57ec59741e3ea8f30000d38d5a9604c6dd756e
Akhila <buraakhila1998@gmail.com>
2022-01-05 15:48:52 +0530

    commit by akhila

--- src/main/java/com/mindtree/cap/Entity/Book.java
+++ src/main/java/com/mindtree/cap/Entity/Book.java
@@ -3,4 +3,13 @@ package com.mindtree.cap.Entity;
 public class Book {
   private int id;
   private String name;
+  private String author;
+public Book(int id, String name, String author) {
+	super();
+	this.id = id;
+	this.name = name;
+	this.author = author;
+}
+  
+}
 

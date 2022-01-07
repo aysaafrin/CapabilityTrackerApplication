170a7c3bf09b50b865b3125c6b13672178753b42
Akhila156 <buraakhila1998@gmail.com>
2022-01-07 16:32:49 +0530

    deleted msg

--- src/main/java/com/mindtree/cap/Entity/Book.java
+++ src/main/java/com/mindtree/cap/Entity/Book.java
@@ -16,7 +16,8 @@ public Book(int id, String name, String author) {
   System.out.println("Hello , I am Akhila");
  System.out.println("Welcome to our repository");
 	  System.out.println("Hello , Afrin");
-	System.out.println("Good Evening");
+	
+	
  
 }
 
